7809b9164bd8d9d3f3cf1d86626a2d2e1f23aae5
Sofia <s.bolesta@yandex.ru>
2023-06-16 15:54:37 +0700

    error correction

--- source/js/gallery-video-swiper.js
+++ source/js/gallery-video-swiper.js
@@ -1,29 +1,30 @@
 import Swiper from '../js/vendor/swiper.js';
+
 const gallerySwiper = document.querySelector('.gallery__swiper');
 
 export const initGallerySwiper = () => {
   if (gallerySwiper) {
     (() =>
-        new Swiper('.gallery__swiper', {
-          direction: 'horizontal',
-          loop: true,
-          effect: 'coverflow',
-          grabCursor: true,
-          centeredSlides: true,
-          slidesPerView: 'auto',
-          coverflowEffect: {
-            rotate: 0,
-            stretch: 270,
-            depth: 200,
-            modifier: 1,
-            slideShadows: false,
-          },
-          spaceBetween: 10,
-          mousewheel: false,
-          keyboard: true,
-          allowSlideNext: true,
-          allowSlidePrev: true,
-        })
+      new Swiper('.gallery__swiper', {
+        direction: 'horizontal',
+        loop: true,
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        coverflowEffect: {
+          rotate: 0,
+          stretch: 270,
+          depth: 200,
+          modifier: 1,
+          slideShadows: false,
+        },
+        spaceBetween: 10,
+        mousewheel: false,
+        keyboard: true,
+        allowSlideNext: true,
+        allowSlidePrev: true,
+      })
     )();
   }
 };
